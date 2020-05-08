f6e102c60fb1189257a89b9212be91f816a9c50b
mindy <mindy@socar.kr>
2020-05-08 15:24:45 +0900

    first init

---
+++ MyFramework/Greeting.swift
@@ -0,0 +1,21 @@
+//
+//  Greeting.swift
+//  MyFramework
+//
+//  Created by mindy on 2020/05/08.
+//  Copyright © 2020 mindy. All rights reserved.
+//
+
+import Foundation
+
+open class Greeting {
+    
+    public init() {
+        
+    }
+    
+    public func greeting() {
+        print("How are you?")
+        print("I'm fine, and you?")
+    }
+}
